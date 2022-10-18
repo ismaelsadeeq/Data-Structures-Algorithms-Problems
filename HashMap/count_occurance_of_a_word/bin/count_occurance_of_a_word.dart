56b8ffda031c01c667a15177dc26634728f7e694
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2022-10-18 17:18:05 +0100

    refactoring Dart count occurrence

--- HashMap/count_occurance_of_a_word/bin/count_occurance_of_a_word.dart
+++
@@ -1,8 +0,0 @@
-import 'package:count_occurance_of_a_word/count_occurance_of_a_word.dart'
-    as hashmap;
-
-void main(List<String> arguments) {
-  String sentence = "The cat ate the dog";
-  Map<String, int> results = hashmap.hashmap(sentence);
-  print("results $results");
-}
--- HashMap/count_occurance_of_a_word/test/count_occurance_of_a_word_test.dart
+++
@@ -1,4 +0,0 @@
-import 'package:count_occurance_of_a_word/count_occurance_of_a_word.dart';
-import 'package:test/test.dart';
-
-void main() {}
