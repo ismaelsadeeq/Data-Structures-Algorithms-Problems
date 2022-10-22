da1af96ddedc197f350b96c59e61fb765306d214
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-10-22 14:18:39 +0100

    Add comments to functions in Stack.c

--- Stack/Array/C/Stack.c
+++ Stack/Array/C/Stack.c
@@ -1,27 +1,46 @@
 #include "Stack.h"
 
+/**
+ * createStack - creates stack of given capacity
+ * @capacity: capacity of the stack
+ * Return: returns pointer to new stack otherwise NULL 
+ */ 
 Stack *createStack(unsigned capacity)
 {
     Stack *stack = malloc(sizeof(Stack));
+    if (Stack == NULL)
+	    return NULL;
     stack->capacity = capacity;
     stack->top = -1;
     stack->array = malloc(stack->capacity * sizeof(int));
     return stack;
 }
 
-
+/**
+ * isFull - check if stack is full
+ * @stack: pointer to the stack to check
+ * Return: returns true if stack is full otherwise false
+ */ 
 int isFull(Stack *stack)
 {
     return stack->top == stack->capacity - 1;
 }
 
-
+/**
+ * isEmpty - check if stack is empty
+ * @stack: pointer to the stack to check
+ * Return: returns true if stack is empty otherwise false
+ */
 int isEmpty(Stack *stack)
 {
     return stack->top == -1;
 }
 
-
+/**
+ * push - add an item to the top of the stack
+ * @stack: pointer to the stack to check
+ * @item: item to add
+ */
 void push(Stack *stack, int item)
 {
     if (isFull(stack))
@@ -30,6 +49,12 @@ void push(Stack *stack, int item)
     printf("%d pushed to stack\n", item);
 }
 
+/**
+ * pop - removes the top item
+ * @stack: pointer to the stack to check
+ * Return: returns the removed item or 
+ * minimum int value if stack is empty
+ */
 int pop(Stack *stack)
 {
     if (isEmpty(stack))
@@ -37,6 +62,12 @@ int pop(Stack *stack)
     return stack->array[stack->top--];
 }
 
+/**
+ * peek - returns top item without removing it
+ * @stack: pointer to the stack to check
+ * Return: returns the top  item or
+ * minimum int value if stack is empty
+ */
 int peek(Stack *stack)
 {
     if (isEmpty(stack))
