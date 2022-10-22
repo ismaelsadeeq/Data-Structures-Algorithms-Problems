6420021de7bb8e35bf9d66ad2d97740e307f5fed
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-10-22 14:00:39 +0100

    Correct Stack.h

--- Stack/Array/C/Stack.h
+++ Stack/Array/C/Stack.h
@@ -8,14 +8,14 @@
 typedef struct Stack {
     int top;
     unsigned capacity;
-    int* array;
+    int *array;
 };
 
 Stack *createStack(unsigned capacity);
-int isFull(struct Stack* stack);
-int isEmpty(struct Stack* stack);
-void push(struct Stack* stack, int item);
-int pop(struct Stack* stack);
-int peek(struct Stack* stack);
+int isFull(Stack *stack);
+int isEmpty(Stack *stack);
+void push(Stack *stack, int item);
+int pop(Stack *stack);
+int peek(Stack *stack);
 
 #endif
